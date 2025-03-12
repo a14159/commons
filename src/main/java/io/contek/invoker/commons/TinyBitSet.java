15672ea6b07afbc2183e385043b1f4408876dfda
kong <kong_k@hotmail.com>
2025-03-12 15:37:02 +0200

    Added TinyBitSet

---
+++ src/main/java/io/contek/invoker/commons/TinyBitSet.java
@@ -0,0 +1,20 @@
+package io.contek.invoker.commons;
+
+public final class TinyBitSet {
+
+    public static long set(long store, int idx) {
+        store |= 1L<<idx;
+
+        return store;
+    }
+
+    public static long clear(long store, int idx) {
+        store &= ~(1L<<idx);
+
+        return store;
+    }
+
+    public static boolean isSet(long store, int idx) {
+        return (store & (1L << idx)) != 0;
+    }
+}
