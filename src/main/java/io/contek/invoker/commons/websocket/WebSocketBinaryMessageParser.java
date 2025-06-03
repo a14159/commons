8b555e76240c738c7c42b119e85516428868f0cd
kong <kong_k@hotmail.com>
2025-06-03 17:29:39 +0300

    added support for binary protocols over WS in WebSocketBinaryMessageParser

--- src/main/java/io/contek/invoker/commons/websocket/WebSocketBinaryMessageParser.java
+++ src/main/java/io/contek/invoker/commons/websocket/WebSocketBinaryMessageParser.java
@@ -14,29 +14,31 @@ public abstract class WebSocketBinaryMessageParser implements IWebSocketMessageP
 
   @Override
   public final ParseResult parse(String text) {
-    throw new UnsupportedOperationException();
-  }
-
-  @Override
-  public final ParseResult parse(byte[] bytes) {
-    String text;
-    try {
-      text = decode(bytes);
-    } catch (Throwable t) {
-      log.error("Failed to decode binary message: size {}.", bytes.length, t);
-      throw new WebSocketIllegalMessageException(t);
-    }
-
     try {
       AnyWebSocketMessage message = fromText(text);
       return new ParseResult(text, message);
     } catch (Throwable t) {
-      log.error("Failed to parse binary message: {}.", text, t);
+      log.error("Failed to parse text message: {}.", text, t);
       throw new WebSocketIllegalMessageException(t);
     }
   }
 
-  protected abstract String decode(byte[] bytes);
+  @Override
+  public final ParseResult parse(byte[] bytes) {
+    try {
+      AnyWebSocketMessage message = fromBytes(bytes);
+      return new ParseResult("binary", message);
+    } catch (Throwable t) {
+      log.error("Failed to decode binary message: size {}.", bytes.length, t);
+      throw new WebSocketIllegalMessageException(t);
+    }
+  }
 
-  protected abstract AnyWebSocketMessage fromText(String text);
+  protected AnyWebSocketMessage fromText(String text) {
+    throw new UnsupportedOperationException();
+  }
+
+  protected AnyWebSocketMessage fromBytes(byte[] bytes) {
+    throw new UnsupportedOperationException();
+  }
 }
